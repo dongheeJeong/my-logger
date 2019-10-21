f7cd2d2777af85a546c00313ca11ed8ab5094dd1
dongheeJeong <38200354+dongheeJeong@users.noreply.github.com>
2019-10-21 15:48:26 +0900

    Add: logger in golang
    유닉스 도메인 소켓을 이용해 프로세스 간 통신을 한다.
    logging메세지를 해당 소켓으로 write하면, 위의 프로그램에서 read하여 logging file에 write한다.
    이 과정에서 하나의 logging file에 synchornized한 write가 가능하도록 프로그램을 작성하였음.

---
+++ logger.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"io"
+	"strings"
+	"time"
+)
+
+const (
+	BUF_SIZE = 1024
+	OPEN_FLAG = os.O_CREATE|os.O_APPEND|os.O_WRONLY
+	UNIX_SOCK = "/tmp/go-unix.sock"
+	LOG_FILE_NAME = "log.out"
+)
+
+func logWriter(msgChan <-chan string) {
+
+	file, err := os.OpenFile(LOG_FILE_NAME, OPEN_FLAG, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	getTimeStamp := func() (stamp string) {
+		/* https://stackoverflow.com/questions/5885486/how-do-i-get-the-current-timestamp-in-go */
+		stamp = time.Now().Format("[2006/01/02 15:04:05]")
+		return
+	}
+
+	/* Read message channel from clientHandler thread forever.*/
+	for msg := range msgChan {
+
+		sentences := strings.Split(msg, "\n");
+
+		for _, sentence := range sentences {
+			if len(sentence) == 0 {
+				continue
+			}
+			file.Write([]byte(getTimeStamp() + sentence + "\n"))
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
+
+func clientHandler(conn net.Conn, msgChan chan<- string) {
+
+	defer conn.Close()
+	bufRecv := make([]byte, BUF_SIZE)
+
+	/* Read message from client until EOF.*/
+	for {
+		n, err := conn.Read(bufRecv)
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			log.Fatal(err);
+		}
+
+		/* Send message to logWriter thread. */
+		if n > 0 {
+			msgChan <- string(bufRecv[:n])
+		}
+	}
+}
+
+func main() {
+
+	/* Delete unix-domain-socket if exists. */
+	if err := os.Remove(UNIX_SOCK); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Listening ... [", UNIX_SOCK, "]");
+
+	/* Create unix-domain-socket. */
+	l, err := net.Listen("unix", UNIX_SOCK)
+	if err != nil {
+		log.Fatal("Listen() err:", err)
+	}
+	defer l.Close()
+
+	/* Channel for client's logging message.*/
+	msgChan := make(chan string, BUF_SIZE)
+
+	/* Start Writer thread */
+	go logWriter(msgChan)
+
+	/* Start accepting clients */
+	for {
+		log.Println("Accepting ...")
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal("Accept() err:", err)
+		}
+		go clientHandler(conn, msgChan)
+	}
+}
